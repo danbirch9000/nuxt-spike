46b8f279001f11ee634063592fb9f218a79658c3
Daniel Birch <dan@Daniels-MacBook-Pro.local>
2018-03-15 20:00:07 +0000

    more work

---
+++ pages/parcel-actions/_barcode/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>parcel-actions barcode: </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default{
+  validate(data){
+    console.log(data);
+    return data.params.barcode === 1;
+
+  }
+}
+</script>

7e5278381137fb12621ac889ce1d1452c56d5894
Daniel Birch <dan@Daniels-MacBook-Pro.local>
2018-03-23 20:43:21 +0000

    added finance and moment

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@
     <div>
       <h1>nuxt-spike</h1>
       <nuxt-link to="/parcel-actions/1">Parcel Actions</nuxt-link>
-      <p>test {{log('this is my message')}}</p>
+      <p>test {{log('this is my message')}} {{getTodaysDate()}}</p>
+      <p>Finance example: {{test1()}}</p>
     </div>
   </section>
 </template>
@@ -11,12 +12,39 @@
 <script>
 import Logo from '~/components/Logo.vue'
 import utilities from '~/common/utilities.js'
+import moment from 'moment'
+const Finance = require('financejs')
+
 export default {
   components: {
     Logo
   },
   methods: {
     log: utilities.writeToPage,
+    getTodaysDate: function() {
+      return moment().format();
+    },
+    test1: function() {
+      console.log(this.calculateSavings(1000, 12));
+      return this.calculateSavings(1000, 12);
+    },
+    calculateSavings(initialAmount, years){
+      var finance = new Finance();
+      var amount = initialAmount;
+      var data = [];
+      for (var index = 0; index < years; index++) {
+        var x = this.getCiforPeriod(amount, index);
+        data.push(x);
+        var interest = x.value - amount;
+        amount = amount + interest;
+      }
+      return data;
+    },
+    getCiforPeriod: function(amount, index){
+      var finance = new Finance();
+      var value = finance.CI(10, 365, amount, 1);
+      return { value: value, year: index + 1, interest: value - amount }
+    }
   }
 }
 </script>

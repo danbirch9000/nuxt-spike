1c5983a1e37d81c59413b97405fb0b1224d099ce
Dan Birch <dan.birch@hermes-europe.co.uk>
2018-03-26 08:02:51 +0100

    added highcharts

--- pages/index.vue
+++ pages/index.vue
@@ -17,31 +17,72 @@
       {{getTodaysDate()}}
       <button v-on:click="saveGoal()">Save</button>
     </div>
+
+      <div id="app">
+        <highcharts :options="options" ref="highcharts"></highcharts>
+      </div>
+
   </section>
+
+
+
+
 </template>
 
 <script>
 
 import utilities from '~/common/utilities.js'
 import moment from 'moment'
-/*
-
-https://www.highcharts.com/demo/spline-irregular-time
-
-{
-        name: 'Winter 2012-2013',
-        // Define the data points. All series have a dummy year
-        // of 1970/71 in order to be compared on the same x axis. Note
-        // that in JavaScript, months start at 0 for January, 1 for February etc.
-        data: [
-            [1522005771, 0],  25315200000
-            [Date.UTC(1970, 10, 4), 0.28],
-            [Date.UTC(1970, 10, 9), 0.25],
-            [Date.UTC(1970, 10, 27), 0.2]
-        ]
+const options = {
+  chart: {
+    type: 'spline'
+  },
+  title: {
+    text: 'Snow depth at Vikjafjellet, Norway'
+  },
+  subtitle: {
+    text: 'Irregular time data in Highcharts JS'
+  },
+  xAxis: {
+    type: 'datetime',
+    dateTimeLabelFormats: { // don't display the dummy year
+        month: '%e. %b',
+        year: '%b'
+    },
+    title: {
+        text: 'Date'
     }
+  },
+  yAxis: {
+    title: {
+        text: 'Snow depth (m)'
+    },
+    min: 0
+  },
+  tooltip: {
+    headerFormat: '<b>{series.name}</b><br>',
+    pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+  },
 
-*/
+  plotOptions: {
+    spline: {
+      marker: {
+          enabled: true
+      }
+    }
+  },
+  series: [
+    {
+      name: 'Winter 2012-2013',
+      data: [
+          [Date.UTC(1970, 9, 21), 0],
+          [Date.UTC(1970, 10, 4), 0.28],
+          [Date.UTC(1970, 10, 9), 0.25],
+          [Date.UTC(1970, 10, 27), 0.2],
+          [Date.UTC(1970, 11, 2), 0.28]
+      ]
+  }]
+  }
 
 export default {
 
@@ -50,7 +91,8 @@ export default {
       rate: 8,
       amount: 17000,
       monthly: 800,
-      years: 10
+      years: 10,
+      options: options
     }
   },
   methods: {
@@ -68,11 +110,11 @@ export default {
       var amount = parseFloat(this.amount);
       var rate = parseFloat(this.rate);
       var years = parseFloat(this.years);
-    
+
       return utilities.calculateSavings(amount, years, monthly, rate, moment().year());
     },
     saveGoal: function(){
-    
+
       this.$store.dispatch('saveGoal', {
       rate: this.rate,
       amount: this.amount,

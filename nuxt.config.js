185df1ea3a962e092d5b251ae9d8283ed9845190
danny_birch@hotmail.com <danny_birch@hotmail.com>
2018-04-16 20:04:06 +0100

    added material design

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,12 +15,23 @@ module.exports = {
     ],
     script: [
       { src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js', integrity: 'sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN', crossorigin: 'anonymous' },
-      { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', integrity: 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl', crossorigin: 'anonymous' }
+      { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', integrity: 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl', crossorigin: 'anonymous' },
+      { src: '/assets/js/core/jquery.min.js' },
+      { src: '/assets/js/core/popper.min.js' },
+      { src: '/assets/js/bootstrap-material-design.js' },
+      { src: '/assets/js/plugins/moment.min.js' },
+      { src: '/assets/js/plugins/bootstrap-datetimepicker.min.js' },
+      { src: '/assets/js/plugins/nouislider.min.js' },
+      { src: '/assets/js/material-kit.js?v=2.0.2' },
+      { src: '/assets/assets-for-demo/js/material-kit-demo.js' }
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' }
-
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons' },
+      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' },
+      { rel: 'stylesheet', href: '/assets/material-kit.min.css?v=2.0.2' }
+      
     ]
   },
 
@@ -32,9 +43,9 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-    '@/assets/styles.scss'
-  ],
+  // css: [
+  //   '@/assets/styles.scss'
+  // ],
 
   /*
   ** Plugins to load before mounting the App

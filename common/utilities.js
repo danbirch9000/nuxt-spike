b70c367f298d98da7e0a04bc538aa1f412d4869a
Dan Birch <dan.birch@hermes-europe.co.uk>
2018-04-17 08:38:00 +0100

    refactor

--- common/utilities.js
+++ common/utilities.js
@@ -1,8 +1,10 @@
 const Finance = require('financejs')
 const moment = require('moment')
 
-module.exports = {
-  getFinanceData: function(rate, amount, monthly, years, startDate) {
+
+var UtilitiesModule = (function () {
+
+  var getFinanceData = function (rate, amount, monthly, years, startDate) {
     if (rate === '' || amount === '' || monthly === '' || years === ''){
       return [];
     }
@@ -10,30 +12,31 @@ module.exports = {
     amount = parseFloat(amount);
     monthly = parseFloat(monthly);
     years = parseFloat(years);
-    return module.exports.calculateSavings(amount, years, monthly, rate, startDate);;
-  },
-  calculateSavings: (initialAmount, years, monthly, rate, date) => {
+    return this.calculateSavings(amount, years, monthly, rate, startDate);
+  };
+
+  var calculateSavings = function (initialAmount, years, monthly, rate, date) {
     var time = moment(date);
     var amount = initialAmount;
     var data = [];
     for (var index = 0; index < years; index++) {
-      var savingsForYear = module.exports.getCiforYear(amount, index, monthly, rate, time.add('years', 1));
-      
+      var savingsForYear = this.getCiforYear(amount, index, monthly, rate, time.add('years', 1));
+
       if (index === 0){
         savingsForYear.totalInterest = savingsForYear.interest;
       }else{
         savingsForYear.totalInterest = data[index - 1].totalInterest + savingsForYear.interest;
       }
-      
-      //savingsForYear.totalInterest = module.exports.numberWithCommas(savingsForYear.totalInterest)
+
+      //savingsForYear.totalInterest = this.numberWithCommas(savingsForYear.totalInterest)
 
       data.push(savingsForYear);
       amount += (savingsForYear.value - amount);
     }
     return data;
-  },
+  };
 
-  getCiforYear: (amount, year, monthly, rate, time) => {
+  var getCiforYear = function (amount, year, monthly, rate, time) {
     var finance = new Finance();
     var principal = amount + monthly;
     var value = 0;
@@ -41,30 +44,41 @@ module.exports = {
       value = finance.CI(rate/12, 1, principal, 1);// rate, compoundings per period, principal, number of periods
       principal = value + monthly;
     };
-    var roundedVal = module.exports.roundToTwo(value);
+    var roundedVal = this.roundToTwo(value);
     return {
       value: roundedVal,
-      formattedvalue: module.exports.numberWithCommas(roundedVal),
+      formattedvalue: this.numberWithCommas(roundedVal),
       date: time.format('MMM YYYY'),
       utc: time.utc().valueOf(),
       interest: module.exports.roundToTwo(value - (amount + (monthly*12)))
     }
-  },
+  };
 
-  roundToTwo: (num) => {
-    return Math.round(num * 100) / 100
-  },
+  var roundToTwo = function (num) {
+    return Math.round(num * 100) / 100;
+  };
 
-  buildChartData: (data) => {
+  var buildChartData = function (data) {
     var chartData = [];
     for (var i = 0; i < data.length; i++) {
       chartData.push([data[i].utc, data[i].value]);
     }
     return chartData;
-  },
+  };
 
-  numberWithCommas(x){
+  var numberWithCommas = function (x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+  };
 
-}
+  return {
+    getFinanceData: getFinanceData,
+    calculateSavings: calculateSavings,
+    getCiforYear: getCiforYear,
+    roundToTwo: roundToTwo,
+    buildChartData: buildChartData,
+    numberWithCommas: numberWithCommas
+  };
+
+})();
+
+module.exports = UtilitiesModule();

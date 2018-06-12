a9ad175af59fd2b7ed0fa14729652428d72f399e
danny_birch@hotmail.com <danny_birch@hotmail.com>
2018-06-12 20:27:53 +0100

    styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -43,19 +43,18 @@ export default {
 
 <style style lang="scss" scoped>
 @import "../assets/colors";
+@import "../assets/mixins";
+
 .body-container {
   position: relative;
   left: 0;
-  -webkit-transition: all 0.25s ease-in-out;
-  -moz-transition: all 0.25s ease-in-out;
-  -o-transition: all 0.25s ease-in-out;
-  transition: all 0.25s ease-in-out;
+  @include transition;
   &.menu-active {
     left: 200px;
-    -webkit-transition: all 0.25s ease-in-out;
-    -moz-transition: all 0.25s ease-in-out;
-    -o-transition: all 0.25s ease-in-out;
-    transition: all 0.25s ease-in-out;
+    @include transition;
+    @media (min-width: 700px) {
+      left: 0;
+    }
   }
 }
 a {
@@ -69,6 +68,9 @@ nav {
   padding: 5px 0;
   border-bottom: 1px #e5e5e5 solid;
   background-color: $white;
+  @media (min-width: 700px) {
+    height: 70px;
+  }
 }
 div.menu {
   position: absolute;
@@ -78,25 +80,50 @@ div.menu {
   background-color: $lightgrey;
   height: 100vh;
   border-right: 1px #e5e5e5 solid;
-}
-ul {
-  width: 100%;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  text-align: left;
-  li {
-    border-bottom: 1px #e5e5e5 solid;
-    padding: 15px 10px;
-    cursor: pointer;
-    &:hover {
-      background-color: darken(#f4f4f4, 5%);
+  @media (min-width: 700px) {
+    position: relative;
+    height: auto;
+    background-color: inherit;
+    left: 0;
+    width: auto;
+    display: inline-block;
+    float: left;
+    margin-left: 0;
+    top: -4px;
+    border: none;
+    border-left: 1px $lightgrey solid;
+    margin-left: 20px;
+    padding-left: 20px;
+  }
+  ul {
+    width: 100%;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    text-align: left;
+    li {
+      border-bottom: 1px #e5e5e5 solid;
+      padding: 14px 10px;
+      cursor: pointer;
+      &:hover {
+        background-color: darken(#f4f4f4, 5%);
+      }
+      @media (min-width: 700px) {
+        float: left;
+        padding: 25px 10px;
+        border: none;
+      }
     }
   }
 }
+
 a.navbar-brand {
   width: 155px;
   display: inline-block;
+  @media (min-width: 700px) {
+    float: left;
+    width: 225px;
+  }
   img {
     width: 100%;
   }
@@ -113,6 +140,9 @@ a.burger-menu {
   background-position-y: 1px;
   cursor: pointer;
   background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHMAAABkCAMAAACCTv/3AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAAZQTFRFAAAA5ubmSUUG+gAAAAJ0Uk5T/wDltzBKAAAAPklEQVR42uzYQQ0AAAgDseHfNC4IyVoD912WAACUm3uampqampqamq+aAAD+IVtTU1NTU1NT0z8EAFBsBRgAX+kR+Qam138AAAAASUVORK5CYII=);
+  @media (min-width: 700px) {
+    display: none;
+  }
 }
 </style>
 

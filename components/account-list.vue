6ce9746405eff0125e4ed819ddfc0b3abab0272a
danny_birch@hotmail.com <danny_birch@hotmail.com>
2018-06-13 19:47:17 +0100

    changed buttons to select

--- components/account-list.vue
+++ components/account-list.vue
@@ -1,29 +1,30 @@
 <template>
     <section>
       <div v-if="getAccounts.length > 0">
-          <span v-for="(item) in getAccounts" :key="item.id">
-            <button @click="loadAccount(item.id)" type="button" class="btn btn-primary btn-sm">{{item.name}}</button>
-          </span>
+          <select v-model="id" @change="loadAccount()">
+            <option v-for="(item) in getAccounts" :value="item.id" :key="item.id">{{item.name}}</option>
+          </select>
       </div>
     </section>
 </template>
 
 <script>
-  export default {
-    data () {
-      return{
-        currentGoal: null
-      }
-    },
-    computed: {
-      getAccounts: function(){
-        return this.$store.state.accountModule.accounts
-      }
-    },
-    methods: {
-      loadAccount(id) {
-        this.$store.commit("SET_ACCOUNT_VIEWING", id);
-      }
+export default {
+  data() {
+    return {
+      currentGoal: null,
+      id: null
+    };
+  },
+  computed: {
+    getAccounts: function() {
+      return this.$store.state.accountModule.accounts;
+    }
+  },
+  methods: {
+    loadAccount() {
+      this.$store.commit("SET_ACCOUNT_VIEWING", this.id);
     }
   }
+};
 </script>

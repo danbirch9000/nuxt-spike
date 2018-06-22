6e5edd82a4162fe935f64b946eaf4805481f39a0
danny_birch@hotmail.com <danny_birch@hotmail.com>
2018-06-22 20:50:09 +0100

    fixed currency filter

--- plugins/money.filter.js
+++ plugins/money.filter.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import utilities from '~/common/utilities.js'
+import Vue from "vue";
+import utilities from "~/common/utilities.js";
 
-Vue.filter('currency', function (value) {
+Vue.filter("currency", function(value) {
   value = utilities.numberWithCommas(value);
+  let digits = value.length - value.indexOf(".");
+  if (digits === 2) {
+    return `£${value}0`;
+  }
   return `£${value}`;
 });
 
-Vue.filter('percentage', function (value) {
-  value = Math.round(value * 100) / 100
+Vue.filter("percentage", function(value) {
+  value = Math.round(value * 100) / 100;
   return `${value} % `;
 });

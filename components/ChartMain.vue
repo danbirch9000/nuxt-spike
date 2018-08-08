f5c9368d1268296691ba8ea0a20ea996f1a04aaf
danny_birch@hotmail.com <danny_birch@hotmail.com>
2018-08-08 20:17:51 +0100

    added all account charts

---
+++ components/ChartMain.vue
@@ -0,0 +1,94 @@
+<template>
+  <section>
+    <div>
+      <highcharts ref="chartComponent" :options="chartConfig" />
+    </div>
+  </section>
+</template>
+
+<script>
+let chartConfig = {
+  chart: {
+    type: "spline",
+    backgroundColor: "#ffffff"
+  },
+  credits: {
+    enabled: false
+  },
+  legend: {
+    enabled: true
+  },
+  title: {
+    text: ""
+  },
+  xAxis: {
+    lineWidth: 0,
+    gridLineWidth: 0,
+    minorGridLineWidth: 0,
+    lineColor: "transparent",
+    type: "datetime",
+    dateTimeLabelFormats: {
+      year: "%Y"
+    },
+    title: {
+      text: "Date"
+    }
+  },
+  yAxis: {
+    lineWidth: 0,
+    gridLineWidth: 0,
+    minorGridLineWidth: 0,
+    lineColor: "transparent",
+    title: {
+      text: "Value"
+    },
+    min: 0
+  },
+  tooltip: {
+    headerFormat: "<b>{series.name}</b><br>",
+    pointFormat: "{point.x:%b %Y}: £{point.y:.2f}"
+  },
+  plotOptions: {
+    spline: {
+      marker: {
+        enabled: false
+      }
+    }
+  },
+  series: []
+};
+
+export default {
+  props: {
+    mainChartData: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    }
+  },
+  data: function() {
+    return {
+      chartConfig: chartConfig,
+      chartData: null,
+      savingsGoal: null,
+      chartShown: true
+    };
+  },
+  watch: {
+    mainChartData() {
+      chartConfig.series = [];
+      this.mainChartData.forEach(element => {
+        chartConfig.series.push({
+          name: element.name,
+          data: element.account,
+          color: "#22cfef"
+        });
+      });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "../assets/colors";
+</style>
--- pages/accounts/index.vue
+++ pages/accounts/index.vue
@@ -5,6 +5,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md">
+          <ChartMain :main-chart-data="accountChartData" />
           <nuxt-link tag="button" to="/new-account" class="btn btn-primary btn-lg btn-block">Create new account</nuxt-link>
           <accountList />
           <accountDetails />
@@ -17,16 +18,55 @@
 <script>
 import accountList from "~/components/account-list";
 import accountDetails from "~/components/account-details";
+import ChartMain from "~/components/ChartMain";
+import { mapState } from "vuex";
+import moment from "moment";
 
 export default {
   middleware: ["check-auth", "auth"],
   components: {
     accountList,
-    accountDetails
+    accountDetails,
+    ChartMain
+  },
+  data() {
+    return {
+      accountChartData: null
+    };
+  },
+  computed: {
+    ...mapState({
+      accounts: state => state.accountModule.accounts
+    })
+  },
+  watch: {
+    accounts() {
+      let allAccounts = [];
+
+      this.accounts.forEach(element => {
+        let temp = {
+          name: element.name,
+          account: []
+        };
+
+        for (const key in element.history) {
+          let momentDate = moment(element.history[key].date);
+
+          temp.account.push([
+            momentDate.utc().valueOf(),
+            Number(element.history[key].value)
+          ]);
+        }
+        allAccounts.push(temp);
+      });
+
+      this.accountChartData = allAccounts;
+    }
   },
   beforeMount() {
     this.$store.commit("CLOSE_MENU");
   },
+  mounted() {},
   created() {
     this.$store.dispatch("GET_USER_ACCOUNTS");
   }

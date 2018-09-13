1d59bff4970902b57cb6400dbcc41f3f1ff0496b
danny_birch@hotmail.com <danny_birch@hotmail.com>
2018-09-13 18:26:27 +0100

    rebuild

--- lambda/config.js
+++ lambda/config.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 218);
+/******/ 	return __webpack_require__(__webpack_require__.s = 299);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 218:
+/***/ 299:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";

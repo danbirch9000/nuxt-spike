f3b58a7b152d2adc9932e80c674da7132a406c2a
danny_birch@hotmail.com <danny_birch@hotmail.com>
2018-09-13 19:44:20 +0100

    chnages

--- lambda/index.js
+++ lambda/index.js
@@ -2110,7 +2110,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/user", function (req, res) {
+app.get("/", function (req, res) {
   res.status(200).send({ test: true });
 });
 
--- netlify/index.js
+++ netlify/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/user", function(req, res) {
+app.get("/", function(req, res) {
   res.status(200).send({ test: true });
 });
 

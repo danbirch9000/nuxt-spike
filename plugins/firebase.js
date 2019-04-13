a01ed801a4a9757fd14b162fadba4375459a2679
danny_birch@hotmail.com <danny_birch@hotmail.com>
2019-04-13 10:16:04 +0100

    Change to firebase key for prod

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,6 +3,6 @@ import { MAIN_CONFIG } from "~/common/env-config";
 
 const config = process.env.isDev
   ? { ...MAIN_CONFIG.dev.firebase }
-  : { ...MAIN_CONFIG.dev.firebase };
+  : { ...MAIN_CONFIG.prod.firebase };
 
 firebase.initializeApp(config);
